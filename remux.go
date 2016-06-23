c1b1a903c774b6929917671df474105b59f940a1
brettaweston <bweston1@gmail.com>
2016-06-23 08:33:46 -0500

    only load strem index if stream exists

--- remux.go
+++ remux.go
@@ -72,8 +72,12 @@ var remuxCommand = cli.Command{
 		ac3Stream := file.AC3Stream()
 		aacStream := file.AACStream()
 		h264TrackIndex := strconv.FormatInt(h264Stream.Index, 10)
-		ac3TrackIndex := strconv.FormatInt(ac3Stream.Index, 10)
-		aacTrackIndex := strconv.FormatInt(aacStream.Index, 10)
+		if ac3Stream != nil {
+			ac3TrackIndex := strconv.FormatInt(ac3Stream.Index, 10)
+		}
+		if aacStream != nil{
+			aacTrackIndex := strconv.FormatInt(aacStream.Index, 10)
+		}
 		convertArgs := []string{"ffmpeg", "-i", input}
 		convertArgs = append(convertArgs, "-map", "0:"+h264TrackIndex)
 		convertArgs = append(convertArgs, "-c:v", "copy")
